6f9f3136bfe9ae372158a5a068ba78d9d234345c
Eryk Ceranek <26123@student.ajp.edu.pl>
2026-03-28 22:08:49 +0100

    Rozbudowanie klasy Book (zmienna wydawnictwo) oraz implemntacja związanych z nią metod.

--- Biblioteka_wspolna2/Program.cs
+++ Biblioteka_wspolna2/Program.cs
@@ -10,22 +10,23 @@ namespace BibliotekaKonsolowa
         public string Title { get; set; }
         public string Author { get; set; }
         public int Year { get; set; }
+        public string Publisher { get; set; } // NOWE
 
         public override string ToString()
         {
-            return $"[{Year}] \"{Title}\" - {Author}";
+            return $"[{Year}] \"{Title}\" - {Author}, {Publisher}";
         }
     }
 
     class Program
     {
         static List<Book> library = new List<Book>();
-
+        //test
         static void Main(string[] args)
         {
             // Przykładowe dane na start
-            library.Add(new Book { Title = "Wiedźmin", Author = "Andrzej Sapkowski", Year = 1993 });
-            library.Add(new Book { Title = "Hobbit", Author = "J.R.R. Tolkien", Year = 1937 });
+            library.Add(new Book { Title = "Wiedźmin", Author = "Andrzej Sapkowski", Year = 1993, Publisher = "SuperNOWA" });
+            library.Add(new Book { Title = "Hobbit", Author = "J.R.R. Tolkien", Year = 1937, Publisher = "Allen & Unwin" });
 
             bool running = true;
             while (running)
@@ -57,12 +58,23 @@ namespace BibliotekaKonsolowa
         {
             Console.Write("Podaj tytuł: ");
             string title = Console.ReadLine();
+
             Console.Write("Podaj autora: ");
             string author = Console.ReadLine();
+
+            Console.Write("Podaj wydawnictwo: "); // NOWE
+            string publisher = Console.ReadLine();
+
             Console.Write("Podaj rok wydania: ");
             if (int.TryParse(Console.ReadLine(), out int year))
             {
-                library.Add(new Book { Title = title, Author = author, Year = year });
+                library.Add(new Book
+                {
+                    Title = title,
+                    Author = author,
+                    Year = year,
+                    Publisher = publisher // NOWE
+                });
                 Console.WriteLine("Książka dodana pomyślnie!");
             }
             else
@@ -111,14 +123,20 @@ namespace BibliotekaKonsolowa
 
         static void SortBooks()
         {
-            Console.WriteLine("Sortuj według: 1.Tytułu, 2.Autora, 3.Roku");
+            Console.WriteLine("Sortuj według: 1.Tytułu, 2.Autora, 3.Roku, 4.Wydawnictwa");
             string choice = Console.ReadLine();
+
             switch (choice)
             {
                 case "1": library = library.OrderBy(b => b.Title).ToList(); break;
                 case "2": library = library.OrderBy(b => b.Author).ToList(); break;
                 case "3": library = library.OrderBy(b => b.Year).ToList(); break;
+                case "4": library = library.OrderBy(b => b.Publisher).ToList(); break; // NOWE
+                default:
+                    Console.WriteLine("Nieprawidłowa opcja!");
+                    break;
             }
+
             Console.WriteLine("Posortowano!");
             Console.ReadKey();
         }
